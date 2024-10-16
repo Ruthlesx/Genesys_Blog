698e4764c15f887a5c14be7183f0e6623075bd36
achykez <chyke.ghoul@gmail.com>
2024-10-16 17:22:24 +0100

    made changes

--- app/page.js
+++ app/page.js
@@ -34,6 +34,8 @@ const page = () => {
             consectetur adipiscing elit.
             Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet.
             Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget arcu.
+
+            
           </p>
         
       
